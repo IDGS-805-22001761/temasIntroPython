7787cef3fed21f78b96644d0ab2c7e569cc90941
IDGS-805-22001761 <78649@alumnos.utleon.edu.mx>
2025-01-14 18:46:47 -0600

    commit del dia 14 de enero, Funciones

---
+++ tem1_variables.py
@@ -0,0 +1,13 @@
+#variables int, float, string, no existe el tipado por defecto
+
+num1=2
+num2=3.5
+nombre="Mario"
+print(num1)
+print(num2)
+print(nombre)
+
+n1="7"
+n2="9"
+suma=int(n1)+int(n2)
+print("La suma es: ",suma)
